2e5e5ca6bf7f5259e9aec6d52786e462a988e75a
Santiago Mendoza <santiagomendozaa6@gmail.com>
2026-02-26 19:35:13 -0400

    Initial Commit

---
+++ VetApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VetApp
+//
+//  Created by Santiago Mendoza on 26/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VetApp/VetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VetAppApp.swift
+//  VetApp
+//
+//  Created by Santiago Mendoza on 26/2/26.
+//
+
+import SwiftUI
+
+@main
+struct VetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
